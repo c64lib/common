930fa96c52bcb635b617d8332292c4f134d42079
Maciej Malecki <maciej.malecki@capgemini.com>
2018-03-29 14:58:30 +0200

    neg function

---
+++ common.asm
@@ -0,0 +1,15 @@
+#importonce
+.filenamespace common 
+
+/*
+ * Why Kickassembler does not support bitwise negation on numerical values?
+ * 
+ * Params:
+ * value: byte to be negated
+ */
+ .function neg(value) {
+	.return value ^ $FF
+}
+.assert "neg($00) gives $FF", neg($00), $FF
+.assert "neg($FF) gives $00", neg($FF), $00
+.assert "neg(%10101010) gives %01010101", neg(%10101010), %01010101
