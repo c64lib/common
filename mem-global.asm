a2c32ddb2aa598442f28010c660c44e1f4f9b3ab
Maciej Malecki <malecki.maciej@gmail.com>
2018-09-07 23:42:02 +0200

    global mem filled

--- mem-global.asm
+++ mem-global.asm
@@ -2,4 +2,9 @@
 #importonce
 .filenamespace c64lib
 
+.macro @copyFast(source, destination, count) { copyFast(source, destination, count) }
+.macro @fillScreen(address, value) { fillScreen(address, value) }
+.macro @set8(value, mem) { set8(value, mem) }
+.macro @set16(value, mem) { set16(value, mem) }
+.macro @copyWordIndirect(source, destinationPointer) { copyWordIndirect(source, destinationPointer) }
 .macro @cmp16(value, low) { cmp16(value, low) } 
