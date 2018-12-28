9d6aa55e3ba583a419e86a944ccd3154e605d264
Maciej Malecki <malecki.maciej@gmail.com>
2018-12-28 17:39:26 +0100

    fill-mem and fill-screen moved to separate files

---
+++ lib/sub/fill-mem.asm
@@ -0,0 +1,31 @@
+#import "../invoke.asm"
+
+/*
+ * Fills memory at given address with given value.
+ * IN:
+ *   A - value
+ *   X - count
+ *   Stack WORD - address
+ * OUT:
+ *   none
+ * MOD: A, X
+ */
+.namespace c64lib {
+  fillMem: {
+
+  sta value + 1 // preserve A for later usage
+  invokeStackBegin(returnPtr)
+  pullParamW(address + 1)
+  
+  value:      lda #$00
+              loop:
+                dex
+  address:      sta $ffff, x
+              bne loop
+              
+  invokeStackEnd(returnPtr)
+  rts
+  // local vars
+  returnPtr: .word 0
+  }
+}
---
+++ lib/sub/fill-screen.asm
@@ -0,0 +1,38 @@
+#import "../invoke.asm"
+#import "../math.asm"
+
+/*
+ * Fills given 1024 bytes of memory with given byte.
+ * IN:
+ *   A - value
+ *   Stack WORD - address
+ * OUT:
+ *   none
+ * MOD: A, X
+ */
+.namespace c64lib {
+  fillScreen: {
+  
+  sta value + 1
+  invokeStackBegin(returnPtr)
+  pullParamWList(List().add(sta0 + 1, sta1 + 1, sta2 + 1, sta3 + 1))
+  add16($0100, sta1 + 1)
+  add16($0200, sta2 + 1)
+  add16($0300, sta3 + 1)
+  
+  value:      lda #$00
+              ldx #$00
+              loop:
+  sta0:         sta $ffff, x
+  sta1:         sta $ffff, x
+  sta2:         sta $ffff, x
+  sta3:         sta $ffff, x
+                inx
+              bne loop
+              
+  invokeStackEnd(returnPtr)
+  rts
+  // local vars
+  returnPtr: .word 0
+  }
+}
