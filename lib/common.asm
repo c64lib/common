0e8f01ea6cb809804b21790f7d25b7eacaf2bdf4
Maciej Malecki <malecki.maciej@gmail.com>
2019-01-19 21:37:16 +0100

    far bne added

--- lib/common.asm
+++ lib/common.asm
@@ -13,3 +13,29 @@
 .assert "neg($00) gives $FF", neg($00), $FF
 .assert "neg($FF) gives $00", neg($FF), $00
 .assert "neg(%10101010) gives %01010101", neg(%10101010), %01010101
+
+/*
+ * "Far" bne branch. Depending on the jump length it either does bne or beq/jmp trick.
+ */
+.pseudocommand fbne label {
+  here:
+    .if (here > label) {
+      // jump back
+      .if (here - label <= 127) {
+        bne label
+       } else {
+        beq skip
+          jmp label
+        skip:
+       }
+    } else {
+      // jump forward
+      .if (label - here <= 128) {
+        bne label
+      } else {
+        beq skip
+          jmp label
+        skip:
+      }
+    }
+}
