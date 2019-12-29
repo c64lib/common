7371ef62eeee40375e9102e4a66331c118ce3ac9
Maciej Malecki <maciej.malecki@capgemini.com>
2019-12-29 19:43:19 +0100

    more tests

--- lib/math-global.asm
+++ lib/math-global.asm
@@ -3,6 +3,7 @@
 .filenamespace c64lib
 
 .macro @c64lib_add16(value, low) { add16(value, low) }
+.pseudocommand @c64lib_add16 value : low { add16 value : low }
 .macro @c64lib_sub16(value, low) { sub16(value, low) }
 .macro @c64lib_addMem16(source, destination) { addMem16(source, destination ) }
 .macro @c64lib_asl16(low) { asl16(low) }
---
+++ spec/math-add16.spec.asm
@@ -0,0 +1,105 @@
+#import "64spec/lib/64spec.asm"
+#import "../lib/math-global.asm"
+#import "../lib/mem-global.asm"
+
+sfspec: init_spec()
+  describe("add16")
+
+  it("0 + 0 = 0"); {
+    c64lib_set16(0, operand)
+    c64lib_set16(0, result)
+    c64lib_set16(0, expected)
+    c64lib_add16 operand : result
+    assert_c_cleared
+    assert_equal16 result : expected
+  }
+  
+  it("1 + 0 = 1"); {
+    c64lib_set16(1, operand)
+    c64lib_set16(0, result)
+    c64lib_set16(1, expected)
+    c64lib_add16 operand : result
+    assert_c_cleared
+    assert_equal16 result : expected
+  }
+
+  it("255 + 1 = 256"); {
+    c64lib_set16(255, operand)
+    c64lib_set16(1, result)
+    c64lib_set16(256, expected)
+    c64lib_add16 operand : result
+    assert_equal16 result : expected
+  }
+
+  it("1 + 255 = 256"); {
+    c64lib_set16(1, operand)
+    c64lib_set16(255, result)
+    c64lib_set16(256, expected)
+    c64lib_add16 operand : result
+    assert_c_cleared
+    assert_equal16 result : expected
+  }
+  
+  it("256 + 0 = 256"); {
+    c64lib_set16(256, operand)
+    c64lib_set16(0, result)
+    c64lib_set16(256, expected)
+    c64lib_add16 operand : result
+    assert_c_cleared
+    assert_equal16 result : expected
+  }
+  
+  it("0 + 256 = 256"); {
+    c64lib_set16(0, operand)
+    c64lib_set16(256, result)
+    c64lib_set16(256, expected)
+    c64lib_add16 operand : result
+    assert_c_cleared
+    assert_equal16 result : expected
+  }
+
+  it("65535 + 0 = 65535"); {
+    c64lib_set16(65535, operand)
+    c64lib_set16(0, result)
+    c64lib_set16(65535, expected)
+    c64lib_add16 operand : result
+    assert_c_cleared
+    assert_equal16 result : expected
+  }
+
+  it("0 + 65535 = 65535"); {
+    c64lib_set16(0, operand)
+    c64lib_set16(65535, result)
+    c64lib_set16(65535, expected)
+    c64lib_add16 operand : result
+    assert_c_cleared
+    assert_equal16 result : expected
+  }
+
+  it("1 + 65535 = 0"); {
+    c64lib_set16(1, operand)
+    c64lib_set16(65535, result)
+    c64lib_set16(0, expected)
+    c64lib_add16 operand : result
+    assert_c_set
+    assert_equal16 result : expected
+  }
+
+  it("65535 + 1 = 0"); {
+    c64lib_set16(65535, operand)
+    c64lib_set16(1, result)
+    c64lib_set16(0, expected)
+    c64lib_add16 operand : result
+    assert_c_set
+    assert_equal16 result : expected
+  }
+finish_spec()
+
+* = * "Data"
+result: .word 0
+expected: .word 0
+operand: .word 0
+
+.print "result=$" + toHexString(result, 4)
+.print "expected=$" + toHexString(expected, 4)
+.print "operand=$" + toHexString(operand, 4)
